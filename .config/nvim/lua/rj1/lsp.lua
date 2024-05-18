0c531faca335b950d6b2855ce7618fb516a987f6
rj1 <rj1@rj1.su>
2024-05-18 11:37:44 -0600

    nvim: add angular snippet support for typescript files

--- .config/nvim/lua/rj1/lsp.lua
+++ .config/nvim/lua/rj1/lsp.lua
@@ -301,6 +301,7 @@ local kind_icons = {
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip").filetype_extend("javascript", { "javascriptreact" })
 require("luasnip").filetype_extend("typescript", { "javascript" })
+require("luasnip").filetype_extend("typescript", { "angular" })
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 cmp.setup({
