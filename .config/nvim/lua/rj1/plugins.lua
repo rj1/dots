7da10175d42634b1f578e87c55256e259f80f38b
rj1 <rj1@rj1.su>
2024-06-28 22:17:13 -0600

    nvim: lua dev setup

--- .config/nvim/lua/rj1/plugins.lua
+++ .config/nvim/lua/rj1/plugins.lua
@@ -98,6 +98,20 @@ require("lazy").setup({
 
 	-- Rename/Delete/Chmod/etc
 	"tpope/vim-eunuch",
+
+	-- lua develpoment
+	{
+  "folke/lazydev.nvim",
+  ft = "lua", -- only load on lua files
+  opts = {
+    library = {
+			"~/play/libuv",
+    },
+    enabled = function(root_dir)
+      return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
+    end,
+  },
+},
 },
 {
 	dev = {
